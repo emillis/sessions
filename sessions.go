17c6d897e35cfb63f4d8fb907306e4d23269d62e
Emilis <emilis.mitrikas@gmail.com>
2022-07-25 20:56:05 +0100

    Made a ton of changes. Maybe I should've submitted them in small chunks. First, added new type, "Requirements". This type will outline the base requirements for a single SessionStore. Now, main access point to sessions will be "SessionStore" instead of "Session". This will allow to spawn multiple caches that will store sessions unrelated to one another. Function "New[TData any](r *Requirements) *SessionStore[TData]" has been edited, now it takes "*Requirements" as an argument and returns pointer to "SessionStore". It's also been made into a generic function that allows to set data type that will be stores as with the session. Removed the global caches responsible for storing sessions, tempUidStore. Also removed DefaultKey global variable. Now it lives within "Requirements" type and will be supplied to a SessionStore if needed. Added unexported global variable, "defaultRequirements". It will outline default requirements if they are not supplied by the client during initiation of a new "SessionStore". Added another unexported field into "Session" type, "data" of generic type TData. This is where any extra data of the session will be stored. Removed "init" function as it's no longer needed since all the global caches have been removed. Instead, local caches for each SessionStore are used and initialized when "New()" is called.

--- sessions.go
+++ sessions.go
@@ -10,28 +10,69 @@ import (
 
 //===========[CACHE/STATIC]=============================================================================================
 
-//Every pointer to a Session structure will be stored here
-var _sessionCache cacheMachine.Cache[string, *Session]
-
-//Only purpose of this cache is to store pointers to Sessions that were modified. This cache is going to be used only
-//for updating the database where instead of saving the entire cache, only the modified ones will be updated
-var _modifiedSessions cacheMachine.Cache[string, *Session]
-
-//When checking for UID existence, possible unique ID will be stored here until determined that it's indeed unique
-var _tmpUidStore cacheMachine.Cache[string, bool]
-
-//DefaultKey is the default key used in key:value pairs such as cookie.Name
-const DefaultKey = "_ssid"
+//If requirements are not supplied, this will be used as default fallback
+var defaultRequirements = Requirements{
+	DefaultKey: "_ssid",
+	Timeout:    0,
+}
 
 //===========[STRUCTURES]===============================================================================================
 
+//Requirements outline the base setup of a SessionStore
+type Requirements struct {
+	DefaultKey string        `json:"default_key" bson:"default_key"`
+	Timeout    time.Duration `json:"timeout" bson:"timeout"`
+}
+
+//Unexported session store where all the related sessions will be cached
+type sessionStore[TData any] struct {
+	//Every pointer to a Session structure will be stored here
+	_sessions cacheMachine.Cache[string, *Session[TData]]
+
+	//Only purpose of this cache is to store pointers to Sessions that were modified. This cache is going to be used only
+	//for updating the database where instead of saving the entire cache, only the modified ones will be updated
+	_modifiedSessions cacheMachine.Cache[string, *Session[TData]]
+
+	//When checking for UID existence, possible unique ID will be stored here until determined that it's indeed unique
+	_tmpUidStore cacheMachine.Cache[string, struct{}]
+
+	//DefaultKey is the default key used in key:value pairs such as cookie.Name
+	Requirements Requirements
+
+	mx sync.RWMutex
+}
+
+//SessionStore is exported access point to all the cached sessions
+type SessionStore[TData any] struct {
+	sessionStore[TData]
+}
+
+//New creates new session in this store with the data supplied and returns pointer to it
+func (ss *SessionStore[TData]) New(data TData) *Session[TData] {
+	uid := generateUid()
+
+	s := &Session[TData]{session[TData]{
+		Uid:  uid,
+		mx:   sync.RWMutex{},
+		data: data,
+	}}
+
+	ss._sessions.AddWithTimeout(uid, s, ss.Requirements.Timeout)
+	ss._modifiedSessions.Add(uid, s)
+
+	return s
+}
+
 //Unexported session definition. Kept private to disable direct access to the session
-type session struct {
+type session[TData any] struct {
 	//This is the unique identifier of the session. It is by default, 99 alphanumeric chars + some special symbols
 	Uid string `json:"uid" bson:"uid"`
 
 	//Key is used in key-value pairs. E.g. It is assigned to cookie.Name
 	Key string `json:"key" bson:"key"`
+
+	//This is where some data is stored for the session
+	data TData
 
 	//Defines a period of time in which the session becomes invalid if not used.
 	//Default value for this is set to 8 hours
@@ -46,20 +87,20 @@ type session struct {
 	mx sync.RWMutex
 }
 
-//Session structure that defines a session
-type Session struct {
-	session
+//Session structure that defines an individual session
+type Session[TData any] struct {
+	session[TData]
 }
 
 //Uid returns unique ID of the session
-func (s *Session) Uid() string {
+func (s *Session[TData]) Uid() string {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
 	return s.session.Uid
 }
 
 //SetUid custom defined Uid for the session
-func (s *Session) SetUid(uid string) {
+func (s *Session[TData]) SetUid(uid string) {
 	s.mx.Lock()
 	s.session.Uid = uid
 	s.mx.Unlock()
@@ -67,14 +108,14 @@ func (s *Session) SetUid(uid string) {
 }
 
 //Key returns session key that can be used as cookie name, etc..
-func (s *Session) Key() string {
+func (s *Session[TData]) Key() string {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
 	return s.session.Key
 }
 
 //SetKey assigns new key for the session
-func (s *Session) SetKey(key string) {
+func (s *Session[TData]) SetKey(key string) {
 	s.mx.Lock()
 	s.session.Key = key
 	s.mx.Unlock()
@@ -82,14 +123,14 @@ func (s *Session) SetKey(key string) {
 }
 
 //TimeoutDuration returns duration in which, if the session is inactive, it goes invalid. Returns duration in seconds
-func (s *Session) TimeoutDuration() time.Duration {
+func (s *Session[TData]) TimeoutDuration() time.Duration {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
 	return s.session.TimeoutDuration
 }
 
 //SetTimeoutDuration sets the duration of time until the session becomes invalid if not used.
-func (s *Session) SetTimeoutDuration(t time.Duration) {
+func (s *Session[TData]) SetTimeoutDuration(t time.Duration) {
 	s.mx.Lock()
 	s.session.TimeoutDuration = t
 	s.mx.Unlock()
@@ -98,20 +139,20 @@ func (s *Session) SetTimeoutDuration(t time.Duration) {
 }
 
 //Valid checks whether the session is still valid
-func (s *Session) Valid() bool {
+func (s *Session[TData]) Valid() bool {
 	return time.Now().Before(s.ValidUntil())
 }
 
 //RefreshTimeout refreshes time left until session goes invalid. It gives the session amount of time defined in
 //SetValidUntil method
-func (s *Session) RefreshTimeout() {
+func (s *Session[TData]) RefreshTimeout() {
 	s.SetValidUntil(time.Now().Add(s.TimeoutDuration()))
 }
 
 //SetSessionCookie sets cookie for the session in the ResponseWriter. The second cookie argument is optional and is used
 //to have some default values set by the client. In essence, this function would override the Name and Value fields of
 //the supplied cookie with the session values
-func (s *Session) SetSessionCookie(w http.ResponseWriter, cookie *http.Cookie) {
+func (s *Session[TData]) SetSessionCookie(w http.ResponseWriter, cookie *http.Cookie) {
 	if cookie == nil {
 		cookie = &http.Cookie{}
 	}
@@ -123,7 +164,7 @@ func (s *Session) SetSessionCookie(w http.ResponseWriter, cookie *http.Cookie) {
 }
 
 //ValidUntil returns timestamp in seconds when this session will become invalid
-func (s *Session) ValidUntil() time.Time {
+func (s *Session[TData]) ValidUntil() time.Time {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
 	return s.session.ValidUntil
@@ -131,7 +172,7 @@ func (s *Session) ValidUntil() time.Time {
 
 //SetValidUntil updates timestamp in seconds when this session will become invalid. It essentially does the calculation
 //(ValidUntil = (current time in seconds) + TimeoutDuration())
-func (s *Session) SetValidUntil(t time.Time) {
+func (s *Session[TData]) SetValidUntil(t time.Time) {
 	s.mx.Lock()
 	s.session.ValidUntil = t
 	s.mx.Unlock()
@@ -139,14 +180,14 @@ func (s *Session) SetValidUntil(t time.Time) {
 }
 
 //LastModified returns time when this session was modified the last
-func (s *Session) LastModified() time.Time {
+func (s *Session[TData]) LastModified() time.Time {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
 	return s.session.LastModified
 }
 
 //UpdateLastModified Sets LastModified field to the time when this function gets invoked
-func (s *Session) UpdateLastModified() {
+func (s *Session[TData]) UpdateLastModified() {
 	s.mx.Lock()
 	defer s.mx.Unlock()
 	s.session.LastModified = time.Now()
@@ -154,33 +195,46 @@ func (s *Session) UpdateLastModified() {
 }
 
 //saveToCache saves this session to local cache
-func (s *Session) saveToCache() {
+func (s *Session[TData]) saveToCache() {
 	_sessionCache.Add(s.Uid(), s)
 }
 
 //removeFromCache deletes the session from local cache
-func (s *Session) removeFromCache() {
+func (s *Session[TData]) removeFromCache() {
 	_sessionCache.Remove(s.Uid())
 	_modifiedSessions.Remove(s.Uid())
 }
 
 //===========[FUNCTIONALITY]============================================================================================
 
-//New creates a new session with default values already pre-filled
-func New() *Session {
-	s := &Session{session{
-		mx:  sync.RWMutex{},
-		Uid: generateUid(),
+//Checks whether Requirements don't have problematic values
+func makeRequirementsReasonable(r *Requirements) *Requirements {
+	if r.DefaultKey == "" {
+		r.DefaultKey = defaultRequirements.DefaultKey
+	}
+
+	if r.Timeout == 0 {
+		r.Timeout = defaultRequirements.Timeout
+	}
+
+	return r
+}
+
+//New initiates and returns a pointer to SessionStore
+func New[TData any](r *Requirements) *SessionStore[TData] {
+	if r == nil {
+		r = &defaultRequirements
+	} else {
+		r = makeRequirementsReasonable(r)
+	}
+
+	s := &SessionStore[TData]{sessionStore[TData]{
+		_sessions:         cacheMachine.New[string, *Session[TData]](nil),
+		_modifiedSessions: cacheMachine.New[string, *Session[TData]](nil),
+		_tmpUidStore:      cacheMachine.New[string, struct{}](nil),
+		Requirements:      *r,
+		mx:                sync.RWMutex{},
 	}}
-
-	s.SetKey("")
-	s.SetTimeoutDuration(time.Second * 28800)
-	s.saveToCache()
-
-	uid := s.Uid()
-
-	_sessionCache.Add(uid, s)
-	_modifiedSessions.Add(uid, s)
 
 	return s
 }
@@ -213,6 +267,7 @@ func doesUidExist(uid string) bool {
 	return false
 }
 
+//TODO: get this to take in SessionStore as an argument so it could find all the existing UIDs
 //Generates and returns new unique UID
 func generateUid() string {
 	for {
@@ -224,12 +279,4 @@ func generateUid() string {
 
 		return newUid
 	}
-}
-
-//===========[INITIALIZATION]===========================================================================================
-
-//Initializing sessions
-func init() {
-	_sessionCache = cacheMachine.New[string, *Session](nil)
-	_modifiedSessions = cacheMachine.New[string, *Session](nil)
 }
